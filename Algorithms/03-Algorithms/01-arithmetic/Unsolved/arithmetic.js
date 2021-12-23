24e735c7b407b8a70aa61729af80ebe0b52e0b3a
Danny Ramirez <danny.ramirezgd@gmail.com>
2021-12-23 15:19:49 -0500

    03 Algorithms solved

--- Algorithms/03-Algorithms/01-arithmetic/Unsolved/arithmetic.js
+++ Algorithms/03-Algorithms/01-arithmetic/Unsolved/arithmetic.js
@@ -1,18 +1,33 @@
 // Fill in the body of each function with the code required
 
 var add = function(num1, num2) {
-    
+    var result = num1 + num2;
+    return result;
 };
 
 var subtract = function(num1, num2) {
-    
+    var result = num1 - num2
+    return result;    
 };
 
 var multiply = function(num1, num2) {
-    
+    var result = num1 * num2;
+    return result;
 };
 
 var divide = function(num1, num2) {
-    
+    var result = num1 / num2;
+    return result;    
 };
+
+add(31,3);
+subtract(10,1);
+multiply(245, 2);
+divide(10,2);
+
+console.log(add(31,3))
+console.log(subtract(10,1))
+console.log(multiply(245,2))
+console.log(divide(10,2))
+
 
