ce6a17d8042f2a5b412e58231face1b4170c06f9
Danny Ramirez <danny.ramirezgd@gmail.com>
2021-12-24 10:42:32 -0500

    05 02 solved

--- Algorithms/05-Algorithms/02-max-num/Unsolved/max-num.js
+++ Algorithms/05-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,8 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    arr.sort(function(a,b){
+        return b-a;
+    });
+    return arr[0];
+};
