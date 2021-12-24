967269f7a0102d9a261ca5171eba149b81b7a4a6
Danny Ramirez <danny.ramirezgd@gmail.com>
2021-12-24 11:00:22 -0500

    05 03 solved

--- Algorithms/05-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ Algorithms/05-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,12 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    result = 0;
+    for (let i = 0; i<str.length; i++){
+        if (str[i] === "a" || str[i] === "e"|| str[i] === "i"|| str[i] === "o"|| str[i] === "u" || 
+        str[i] === "A" || str[i] === "E"|| str[i] === "I"|| str[i] === "O"|| str[i] === "U"){
+            result++;
+        }
+    }
+    return(result);
+};
